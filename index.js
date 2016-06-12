0bbcfd9737c183cc3145688226e7335f8e2ef167
Sebastian Motraghi <sebm@users.noreply.github.com>
2016-06-11 23:13:19 -0700

    express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("started from the bottom");
+var express = require('express');
+var app = express();
+
+var PORT = '3030';
+
+app.get('/', function(req, res) {
+	res.send('Hey');
+})
+
+app.listen(PORT, function() {
+	console.log(`listening on ${PORT}`);
+});

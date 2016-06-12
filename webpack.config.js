5d1580737cba09124e6fb79a990dade234c7d926
Sebastian Motraghi <sebm@users.noreply.github.com>
2016-06-12 00:17:07 -0700

    chhhaannggess

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ var BUILD_DIR = path.resolve(__dirname, 'static');
 var APP_DIR = path.resolve(__dirname, 'client/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
+  entry: [
+    APP_DIR + '/index.jsx'
+  ],
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'

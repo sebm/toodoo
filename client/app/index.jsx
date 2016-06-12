333e05aafe1b5b738a9e6bed30a923d4e333883b
Sebastian Motraghi <sebm@users.noreply.github.com>
2016-06-12 00:38:01 -0700

    add some elements

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+class TodoItems extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>Fake entry</li>
+      </ul>
+    );
+  }
+}
+
+class TodoForm extends React.Component {
+  render() {
+    return (
+      <form>
+        <input />
+        <button />
+      </form>
+    )
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <div className="TodoList">
+        <TodoForm />
+        <TodoItems />
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
 	render () {
-		return <p>Hello React!</p>;
+		return (
+      <div className="AppContainer">
+        <h1>Toodoo</h1>
+        <TodoList />
+      </div>
+    );
 	}
 }
 
